0201594a9216a76db9a72725ea4fa2e573fbfde0
rayel_ahmed <rayelahhmed404@gmail.com>
2023-03-26 14:49:28 +0600

    8th commit

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run() {
       res.send(result);
     });
 
+    // Update Data:
     app.put("/product/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
